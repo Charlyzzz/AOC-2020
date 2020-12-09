a3af02e01b075135c4467638669e79516bdf1ceb
Charlyzzz <erwincdl@gmail.com>
2020-12-08 21:46:00 -0300

    tailrec optimizations

--- src/main/kotlin/day7/LuggageProcessing.kt
+++ src/main/kotlin/day7/LuggageProcessing.kt
@@ -1,6 +1,6 @@
 package day7
 
-import java.util.*
+import print
 
 val isEmpty = Regex("(.*) bags contain no other bags")
 val bagColor = Regex("""(\w+ \w+) bags contain""")
@@ -8,76 +8,81 @@ val bagContent = Regex("""(\d+) (\w+ \w+)""")
 
 typealias Color = String
 
+data class Content(val color: Color, val count: Int)
+
+data class Bag(val color: Color, val contains: List<Content>, val containedBy: List<Color>)
+
 private const val SHINY_GOLD = "shiny gold"
 
 fun main() {
-    val (contentMap, reverseContentMap) = buildBagMap(bagRules)
-    countBagsThatContainsGoldenBag(reverseContentMap).also { println(it) }
-    countBagsInsideGoldenBag(contentMap).also { println(it) }
+    val bagGraph = buildBagGraph(bagRules)
+    countBagsThatContainsGoldenBags(bagGraph).print()
+    countBagsInsideGoldenBag(bagGraph).print()
 }
 
-fun countBagsInsideGoldenBag(contentMap: Map<Color, List<Pair<Color, Int>>>): Int =
-    countBagsInside(contentMap, SHINY_GOLD)
-
-fun countBagsInside(contentMap: Map<Color, List<Pair<Color, Int>>>, color: Color): Int {
-    val content = contentMap.getOrDefault(color, emptyList())
-    if (content.isEmpty()) {
-        return 0
-    }
-    return content.sumBy { (newColor, count) ->
-        count + count * countBagsInside(contentMap, newColor)
-    }
+private fun countBagsThatContainsGoldenBags(bagGraph: Map<Color, Bag>): Int {
+    val initialBags = bagGraph.getValue(SHINY_GOLD).containedBy
+    return countBagsContainedBy(bagGraph, emptySet(), initialBags).size
 }
 
-fun countBagsThatContainsGoldenBag(reverseContentMap: Map<Color, Set<Color>>): Int {
-    var bagsVisited = emptySet<Color>()
-    val initialBags = reverseContentMap[SHINY_GOLD] ?: error("color not found")
-    val bagsToCheck = emptyQueue(initialBags)
-    while (bagsToCheck.isNotEmpty()) {
-        bagsToCheck.poll().also {
-            bagsVisited = bagsVisited + it
-            val colors = reverseContentMap.getOrDefault(it, emptySet())
-            colors.forEach { bagToCheck ->
-                bagsToCheck.add(bagToCheck)
-            }
-        }
-    }
-    return bagsVisited.size
+private tailrec fun countBagsContainedBy(
+    bagGraph: Map<String, Bag>,
+    colorsChecked: Set<Color>,
+    nextColors: List<Color>
+): Set<Color> {
+    if (nextColors.isEmpty()) return colorsChecked
+    val color = nextColors.first()
+    val rest = nextColors.drop(1)
+    val bag = bagGraph.getValue(color)
+    return countBagsContainedBy(bagGraph, colorsChecked + color, rest + bag.containedBy)
 }
 
+data class NextBag(val content: Content, val multiplier: Int)
 
-data class BagMap(val contentMap: Map<Color, List<Pair<Color, Int>>>, val reverseContentMap: Map<Color, Set<Color>>)
+private fun countBagsInsideGoldenBag(bagGraph: Map<Color, Bag>): Int {
+    val initialBags = nextBags(bagGraph, SHINY_GOLD, 1)
+    return countBagsInside(bagGraph, 0, initialBags)
+}
 
-fun buildBagMap(bagRules: String): BagMap =
-    bagRules.lines().fold(BagMap(emptyMap(), emptyMap())) { bagMap, rule ->
+private tailrec fun countBagsInside(bagGraph: Map<Color, Bag>, bagCount: Int, nextBags: List<NextBag>): Int {
+    if (nextBags.isEmpty()) return bagCount
+    val nextBag = nextBags.first()
+    val rest = nextBags.drop(1)
+    val (content, multiplier) = nextBag
+    val (color, count) = content
+    return countBagsInside(
+        bagGraph,
+        bagCount + count * multiplier,
+        rest + nextBags(bagGraph, color, count * multiplier)
+    )
+}
+
+private fun nextBags(bagGraph: Map<Color, Bag>, color: Color, multiplier: Int): List<NextBag> {
+    return bagGraph.getValue(color).contains.map { NextBag(it, multiplier) }
+}
+
+private fun buildBagGraph(bagRules: String): Map<Color, Bag> =
+    bagRules.lines().fold(emptyMap()) { graph, rule ->
         when {
             isEmpty.matches(rule) -> {
                 val color = isEmpty.find(rule)!!.groupValues.first()
-                bagMap.copy(
-                    contentMap = bagMap.contentMap + (color to emptyList()),
-                    reverseContentMap = bagMap.reverseContentMap + (color to emptySet())
-                )
+                val bag = graph[color] ?: Bag(color, emptyList(), emptyList())
+                graph + (color to bag)
             }
             else -> {
-                val bagColor = bagColor.find(rule)!!.groupValues[1]
-                bagContent.findAll(rule).fold(bagMap) { bm, match ->
-                    val (_, count, bagInside) = match.groupValues
-                    val bagContent = bm.contentMap.getOrDefault(bagColor, emptyList()) + (bagInside to count.toInt())
-
-                    val existingBags = bm.reverseContentMap.getOrDefault(bagInside, emptySet())
-
-                    bm.copy(
-                        contentMap = bm.contentMap + (bagColor to bagContent),
-                        reverseContentMap = bm.reverseContentMap + (bagInside to (existingBags + bagColor))
-                    )
+                var updatedGraph = graph
+                val color = bagColor.find(rule)!!.groupValues[1]
+                val content = bagContent.findAll(rule).fold(emptyList<Content>()) { content, match ->
+                    val (_, count, insideBagColor) = match.groupValues
+                    val insideBag = graph[insideBagColor] ?: Bag(insideBagColor, emptyList(), emptyList())
+                    updatedGraph =
+                        updatedGraph + (insideBagColor to insideBag.copy(
+                            containedBy = insideBag.containedBy + color
+                        ))
+                    content + Content(insideBagColor, count.toInt())
                 }
+                val bag = graph[color] ?: Bag(color, emptyList(), emptyList())
+                updatedGraph + (color to bag.copy(contains = content))
             }
         }
-    }
-
-fun <T> emptyQueue(elems: Collection<T>? = null): Queue<T> =
-    if (elems.isNullOrEmpty()) {
-        LinkedList()
-    } else {
-        LinkedList(elems)
     }
